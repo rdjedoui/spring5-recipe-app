34189e64fb64c37eea49cfa8651bc05e6ab0dc74
rdjedoui <rafika.djedoui@gmail.com>
2019-08-11 17:58:09 +0200

    Database Initialization with Spring

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,5 +1,7 @@
 package guru.springframework.controllers;
 
+import guru.springframework.repositories.CategoryRepository;
+import guru.springframework.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,8 +11,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
+    CategoryRepository categoryRepository;
+    UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage(){
+
+        System.out.println(categoryRepository.findByDescription("American").get().getId());
+        System.out.println(unitOfMeasureRepository.findByDescription("Teaspoon").get().getId());
         return "index";
     }
 }
